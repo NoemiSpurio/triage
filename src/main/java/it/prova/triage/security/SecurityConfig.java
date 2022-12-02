afee4c5f18c67e1602c65c19b788a6f461283aeb
NoemiSpurio <noemi.spurio@gmail.com>
2022-12-02 10:29:54 +0100

    implementati filtri sicurezza

--- src/main/java/it/prova/triage/security/SecurityConfig.java
+++ src/main/java/it/prova/triage/security/SecurityConfig.java
@@ -44,8 +44,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 				.authorizeHttpRequests().antMatchers("/api/auth/login").permitAll()
 				// tutti gli utenti autenticati possono richiedere le info
-				.antMatchers("/api/utente/userInfo").authenticated().antMatchers("/api/utente/**").hasRole("ADMIN")
-				.antMatchers("/**").hasAnyRole("ADMIN", "CLASSIC_USER")
+				.antMatchers("/api/utente/**").hasRole("ADMIN")
+				.antMatchers("/**").authenticated()
 				// .antMatchers("/anonymous*").anonymous()
 				.anyRequest().authenticated().and()
 
